cc0bcbd71fdd36779a9b0a527340cd8dad146a0c
standorsett <stan.dorsett@gmail.com>
2014-02-15 21:51:07 -0600

    adding razorsedge/network and jonnyx/resolv_conf modules

---
+++ manifests/init.pp
@@ -0,0 +1,4 @@
+class { "network": }
+class { 'network::global': hostname => 'pwmgmt.oakclifflabs.net', } 
+network::if::static { 'eth0': ensure => 'up', ipaddress => '192.168.1.16', netmask => '255.255.255.0', gateway => '192.168.1.1', } 
+class { 'resolv_conf': nameserver => [ '192.168.1.5', '192.168.1.1' ], domain => 'oakclifflabs.net', } 
